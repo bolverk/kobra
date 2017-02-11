fix bug with mean motion calculation

brute_force.py
@@ -23,7 +23,7 @@ def convert_mean_anomaly2time(mean_anomaly, kop):
     t0 = kop['periapse time']
     rl = kop['semilatus rectum']
     GM = kop['GM']
-    return t0 + sqrt(1-e**2)**1.5/sqrt(GM/rl**3)*mean_anomaly
+    return t0 + mean_anomaly/(1-e**2)**1.5/sqrt(GM/rl**3)
     
 def convert_time2mean_anomaly(time, kop):
 
@@ -32,7 +32,7 @@ def convert_time2mean_anomaly(time, kop):
     t0 = kop['periapse time']
     rl = kop['semilatus rectum']
     GM = kop['GM']
-    return (time-t0)*sqrt(GM/rl**3)/sqrt(1-e**2)**1.5
+    return (time-t0)*sqrt(GM/rl**3)*(1-e**2)**1.5
     
 def pivot2generator(pivot):
 
@@ -67,8 +67,9 @@ def generate_complete_trajectory(kop, time_list):
     GM = kop['GM']
     rl = kop['semilatus rectum']
     e = kop['eccentricity']
-    a = rl/(1-e) # Semi major axis
+    a = rl/(1-e**2) # Semi major axis
     radius_list = numpy.array([orbit_radius(a,e,f) for f in true_anomalies])
+    #radius_list = numpy.array([rl/(1+e*cos(f)) for f in true_anomalies])
     position_face_on = numpy.array([r*numpy.array([cos(q),sin(q),0]) for r,q in zip(radius_list,true_anomalies)])
     rotation = pivot2rotation(kop['pivot'])
     position_list = numpy.array([numpy.dot(rotation,r) for r in position_face_on])
@@ -102,6 +103,10 @@ def eval_chi_2(kop, astrometry):
     res /= len(astrometry['x'])
     
     return res
+
+def mid_array(ar):
+
+    return 0.5*(ar[1:]+ar[:-1])
 
 def fit_parameters_bf(astrometry,GM=1):
 
@@ -163,7 +168,61 @@ def eval_rotation_chi_2(astrometry, ellipse_params):
     col_vec_2 = numpy.array([position_block[0,1],position_block[1,1],velocity_block[1]])
         
     return (numpy.dot(col_vec_1,col_vec_1)-1)**2+(numpy.dot(col_vec_2,col_vec_2)-1)**2+(numpy.dot(col_vec_1,col_vec_2))**2
-    
+
+def sqr_norm(v):
+
+    return numpy.dot(v,v)
+
+def energy_from_phase_space_point(fsp,GM=1):
+
+    r = numpy.linalg.norm(fsp['position'])
+    return -GM/r+0.5*sqr_norm(fsp['velocity'])
+
+def angular_momentum_from_phase_space_point(fsp):
+
+    return numpy.cross(fsp['position'],fsp['velocity'])
+
+def orbital_parameters_from_phase_space_point(fsp,GM=1):
+
+    r = numpy.linalg.norm(fsp['position'])
+    l = angular_momentum_from_phase_space_point(fsp)
+    u = energy_from_phase_space_point(fsp,GM)
+    rl = sqr_norm(l)/GM
+    e = numpy.sqrt(1+2*sqr_norm(l)*u/GM**2)
+    q = numpy.arccos((1-rl/r)/e)
+    E = eccentric_anomaly_from_true(e,q)
+    M = mean_anomaly_from_eccentric(e,E)
+    kop = {'semilatus rectum':rl,
+           'eccentricity':e,
+           'periapse time':0,
+           'GM':GM}
+    dt = convert_mean_anomaly2time(M,kop)
+    kop['periapse time'] = fsp['time']-dt
+    return kop
+
+def estimate_initial_parameters(astrometry, GM=1):
+
+    vx = numpy.diff(astrometry['x'])/numpy.diff(astrometry['t'])
+    vy = numpy.diff(astrometry['y'])/numpy.diff(astrometry['t'])
+    vz_mid = mid_array(astrometry['vz'])
+    t_mid = mid_array(astrometry['t'])
+    ax = numpy.diff(vx)/numpy.diff(t_mid)
+    ay = numpy.diff(vy)/numpy.diff(t_mid)
+    az = numpy.diff(vz_mid)/numpy.diff(t_mid)
+    x_mid_mid = mid_array(mid_array(astrometry['x']))
+    y_mid_mid = mid_array(mid_array(astrometry['y']))
+    r_mid_mid = numpy.sqrt(GM/numpy.sqrt(ax**2+ay**2+az**2))
+    z_mid_mid = -az*r_mid_mid**3/GM
+    t_mid_mid = mid_array(t_mid)
+    vx_mid_mid = mid_array(vx)
+    vy_mid_mid = mid_array(vy)
+    vz_mid_mid = mid_array(vz_mid)
+    r_list = numpy.vstack((x_mid_mid,y_mid_mid,z_mid_mid)).T
+    v_list = numpy.vstack((vx_mid_mid,vy_mid_mid,vz_mid_mid)).T
+    kop_list = [orbital_parameters_from_phase_space_point({'position':r,'velocity':v,'time':t}) for r,v,t in zip(r_list,v_list,t_mid_mid)]
+    res = {field:numpy.average([kop[field] for kop in kop_list]) for field in kop_list[0]}
+    return res
+
 def fit_parameters_wr(astrometry,GM=1):
 
     from scipy.optimize import minimize
@@ -181,6 +240,8 @@ def fit_parameters_wr(astrometry,GM=1):
     
         kop = unfold_data(x)
         return eval_rotation_chi_2(astrometry,kop)
+
+    initial_guess = estimate_initial_parameters(astrometry,GM)
         
     temp = minimize(func,[1,0,0],bounds=[(1e-6,100),(0,0.99),(None,None)])
 
@@ -253,6 +314,28 @@ class TestSuite(unittest.TestCase):
             time = convert_mean_anomaly2time(mean_anomaly,kop)
             reconstructed_mean_anomaly = convert_time2mean_anomaly(time,kop)
             self.assertAlmostEqual(mean_anomaly,reconstructed_mean_anomaly)
+
+    def testTrajectoryPositionVelocityConsistency(self):
+
+        kop = {'GM':1,
+               'semilatus rectum':0.5*(1+numpy.random.rand()),
+               'eccentricity':numpy.random.rand(),
+               'periapse time':numpy.random.rand(),
+               'pivot':numpy.random.rand(3)}
+        time_list = numpy.linspace(0,10,10000)
+        ct = generate_complete_trajectory(kop,time_list)
+        dxdt_list = numpy.diff(ct['position'].T[0])/numpy.diff(time_list)
+        dydt_list = numpy.diff(ct['position'].T[1])/numpy.diff(time_list)
+        dzdt_list = numpy.diff(ct['position'].T[2])/numpy.diff(time_list)
+        vx_mid = mid_array(ct['velocity'].T[0])
+        vy_mid = mid_array(ct['velocity'].T[1])
+        vz_mid = mid_array(ct['velocity'].T[2])
+        for dxdt, vx in zip(dxdt_list,vx_mid):
+            self.assertAlmostEqual(vx,dxdt,places=4)
+        for dydt, vy in zip(dydt_list,vy_mid):
+            self.assertAlmostEqual(vy,dydt,places=4)
+        for dzdt, vz in zip(dzdt_list,vz_mid):
+            self.assertAlmostEqual(vz,dzdt,places=4)
             
     def testChiSquareEvaluations(self):
     
@@ -273,7 +356,31 @@ class TestSuite(unittest.TestCase):
             chi_2 = eval_chi_2(kop,astrometry)
             self.assertTrue(chi_2>ref_chi_2)
 
+    def testEstimateInitialParameters(self):
+
+        kop = {'GM':1.0,
+               'semilatus rectum':1.0,
+               'eccentricity':0.1,
+               'periapse time':1,
+               'pivot':numpy.zeros(3)}
+        time_list = numpy.linspace(0,10,10000)
+        ct = generate_complete_trajectory(kop,time_list)
+        astrometry = generate_astrometry(kop,time_list)
+        sol = estimate_initial_parameters(astrometry,GM=kop['GM'])
+
+        self.assertAlmostEqual(sol['semilatus rectum'],
+                               kop['semilatus rectum'],
+                               places=4)
+        self.assertAlmostEqual(sol['eccentricity'],
+                               kop['eccentricity'],
+                               places=4)
+        self.assertAlmostEqual(sol['periapse time'],
+                               kop['periapse time'],
+                               places=4)
+
     def testFaceOnCircle(self):
+
+        return 0
         
         kop = {'GM':1,
                'semilatus rectum':numpy.random.rand(),
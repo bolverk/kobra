2847ececc22a709d6d53dcce198651948698ebdc
Almog Yalinewich <almog.yalin@gmail.com>
2017-02-17 12:51:36 +0200

    add documentation

--- brute_force.py
+++ brute_force.py
@@ -21,9 +21,25 @@ lcs = numpy.array([[[Eijk(i,j,k) for k in range(3)]
 
 def sqr_norm(v):
 
+    """
+    Calculates the square of the norm of a vector.
+    Basically I wanted to avoid writing the same vector twice.
+
+    :param v: Vector
+    :return: Scalar
+    """
+
     return numpy.dot(v,v)
 
 def calc_best_cayley_rotation(x_list, y_list):
+
+    """
+    Given two sets of ordered/labelled vectors, finds the rotation matrix that brings them close together
+
+    :param x_list: First list of vectors
+    :param y_list: Second list of vectors
+    :return: Pivot vector
+    """
 
     mat = numpy.identity(3)*numpy.sum(sqr_norm(x+y) for x,y in zip(x_list,y_list)) - numpy.sum(numpy.kron(x+y,x+y) for x,y in zip(x_list,y_list)).reshape((3,3))
     vec = numpy.sum((numpy.cross(x,y) for x,y in zip(x_list,y_list)))
@@ -31,6 +47,14 @@ def calc_best_cayley_rotation(x_list, y_list):
     return 2*numpy.dot(numpy.linalg.inv(mat),vec)
 
 def solve_kepler_equation(e,M):
+
+    """
+    Calculates the eccentric anomaly by solving Kepler's equation.
+
+    :param e: Eccentricity
+    :param M: Mean anomaly
+    :return: Eccentric anomaly (a scalar)
+    """
 
     from scipy.optimize import fsolve
 
@@ -44,6 +68,14 @@ def solve_kepler_equation(e,M):
     
 def convert_mean_anomaly2time(mean_anomaly, kop):
 
+    """
+    Converts mean anomaly to time. This is the reverse of :py:meth:`brute_force.convert_time2mean_anomaly`
+    
+    :param mean_anomaly: Mean anomaly
+    :param kop: Keplerian orbit parameters
+    :return: Time (scalar)
+    """
+
     sqrt = numpy.sqrt
     e = kop['eccentricity']
     t0 = kop['periapse time']
@@ -52,6 +84,14 @@ def convert_mean_anomaly2time(mean_anomaly, kop):
     return t0 + mean_anomaly/(1-e**2)**1.5/sqrt(GM/rl**3)
     
 def convert_time2mean_anomaly(time, kop):
+
+    """
+    Converts time to mean anomaly. This is the reverse of :py:meth:`brute_force.convert_mean_anomaly2time`
+
+    :param time: Time
+    :param kop: Keplerian orbit parameters
+    :return: Mean anomaly (scalar)
+    """
 
     sqrt = numpy.sqrt
     e = kop['eccentricity']
@@ -62,9 +102,23 @@ def convert_time2mean_anomaly(time, kop):
     
 def pivot2generator(pivot):
 
+    """
+    Maps a three dimensional vector to an anti-symmetric matrix by tensor multiplication with the Levi - Civita Symbol. This is the reverse of :py:meth:`brute_force.generator2pivot`
+
+    :param pivot: Pivot vector
+    :return: Anti - symmetric matrix
+    """
+
     return -numpy.einsum('ijk,k',lcs,pivot)
 
 def calc_pivot_from_gl_rectangle_block(block):
+
+    """
+    Calculates the rotation based on the information from the rectangular matrix block given from the astrometric fit.
+
+    :param block: General linear 2x3 matrix 
+    :return: Pivot vector
+    """
 
     proj = numpy.zeros((2,3))
     proj[0,0] = 1
@@ -76,9 +130,23 @@ def calc_pivot_from_gl_rectangle_block(block):
 
 def generator2pivot(generator):
 
+    """
+    Maps an anti - symmetric matrix to a pivot vector. This is the reverse of :py:meth:`brute_force.pivot2generator`
+    
+    :param generator: An anti symmetric matrix
+    :return: Pivot vector
+    """
+
     return -0.5*numpy.einsum('ijk,jk',lcs,generator)
     
 def generator2rotation(generator):
+
+    """
+    Calculates a rotation matrix from an anti symmetric generator using the Cayley transformation
+
+    :param generator: Anti - symmetric generator matrix
+    :return: Rotation matrix
+    """
 
     g = generator
     g2 = numpy.dot(g,g)
@@ -86,9 +154,23 @@ def generator2rotation(generator):
     
 def pivot2rotation(pivot):
 
+    """
+    Calculates the rotation matrix from a pivot vector. This is the reverse of :py:meth:`brute_force.rotation2pivot`
+    
+    :param pivot: Pivot vector
+    :return: Rotation matrix
+    """
+
     return generator2rotation(pivot2generator(pivot))
 
 def rotation2pivot(rotation):
+
+    """
+    Calculates the pivot vector from a rotation. This is the reverse of :py:meth:`brute_force.pivot2rotation`
+
+    :param rotation: Rotation matrix
+    :return: Pivot vector
+    """
 
     i3 = numpy.identity(3)
     aux = numpy.dot(rotation+i3,
@@ -98,6 +180,14 @@ def rotation2pivot(rotation):
         generator2pivot(rotation))
     
 def generate_complete_trajectory(kop, time_list):
+
+    """
+    Creates a list of positions and velocities of an object along a Keplerian orbit
+
+    :param kop: Keplerian orbit parameters
+    :param time_list: List of times when the positions and velocities will be evaluated
+    :return: A dictionary with two entries: "positions", with a list of 3D positions, and "velocities" with a list of 3D velocities
+    """
 
     mean_anomalies = convert_time2mean_anomaly(time_list,kop)
     #eccentric_anomalies = numpy.array([eccentric_anomaly_from_mean(kop['eccentricity'],m,tolerance=abs(1e-7*m)) for m in mean_anomalies])
@@ -134,6 +224,14 @@ def generate_complete_trajectory(kop, time_list):
     
 def generate_astrometry(kop,time_list):
 
+    """
+    Simulates observational data.
+
+    :param kop: Keplerian orbit parameters
+    :param time_list: List of observation times
+    :return: A dictionary with four entries: "t" - measurement times, "x" - x component of the position, "y" - y component of the position and "vz" - radial velocity component
+    """
+
     trajectory = generate_complete_trajectory(kop,time_list)
     
     return {'t':time_list,
@@ -142,6 +240,14 @@ def generate_astrometry(kop,time_list):
             'vz':trajectory['velocity'].T[2]}
     
 def eval_chi_2(kop, astrometry):
+
+    """
+    Evaluates how well a certain Keplerian orbit fits observational data
+
+    :param kop: Keplerian orbit parameters
+    :param astrometry: Astrometric data (proper motion and radial velocity)
+    :return: Chi squared value
+    """
 
     reproduction = generate_astrometry(kop, astrometry['t'])
     
@@ -161,9 +267,24 @@ def eval_chi_2(kop, astrometry):
 
 def mid_array(ar):
 
+    """
+    Linearly interpolates values between consecutive entries
+
+    :param ar: Original array
+    :return: Array whose length is shorter by 1 from the length of ar
+    """
+
     return 0.5*(ar[1:]+ar[:-1])
 
 def fit_small_rotation(astrometry, trajectory):
+
+    """
+    Finds a rotation that best aligns a complete theoretical trajectory with observed data, assuming the necessary rotation is small (rotation angle much smaller than 1)
+    
+    :param astrometry: Astrometric data (proper motion and radial velocity)
+    :param trajectory: Complete trajectory
+    :return: Pivot vector
+    """
 
     W = numpy.array([[1,0,0],[0,1,0],[0,0,0]])
     Z = numpy.array([[0,0,0],[0,0,0],[0,0,1]])
@@ -183,9 +304,18 @@ def fit_small_rotation(astrometry, trajectory):
     v2 = numpy.einsum('ijk,jl,nl,nk',lcs,Z,v_list-u_list,u_list)/len(u_list)
     
     return -0.5*numpy.dot(numpy.linalg.inv(m1+m2),
-                       v1+v2)
+                          v1+v2)
 
 def fit_rotation_to_astrometry(astrometry, trajectory, n_itr=3):
+
+    """
+    Finds the rotation necessary to align a theoretical trajectory with observations. 
+
+    :param astrometry: Astrometric data (proper motion and radial velocity)
+    :param trajectory: Complete theoretical trajectory
+    :param n_itr: Number of iteration
+    :return: Pivot vector
+    """
 
     block = numpy.vstack((
         calc_gl_position_block(astrometry,trajectory),
@@ -201,6 +331,14 @@ def fit_rotation_to_astrometry(astrometry, trajectory, n_itr=3):
     return pivot
 
 def fit_parameters_bf(astrometry,GM=1):
+
+    """
+    Fits Keplerian parameters to astrometric data using a "brute force" approach, i.e. fits all 6 parameters simultaneously.
+
+    :param astrometry: Astrometric data (proper motion and radial velocity)
+    :param GM: Value of the product G (universal constant of gravity) and M (mass of the central gravitating mass)
+    :return: Keplerian orbit parameters
+    """
 
     from scipy.optimize import minimize
     
@@ -229,6 +367,14 @@ def fit_parameters_bf(astrometry,GM=1):
     return unfold_data(temp.x)
     
 def eval_rotation_chi_2(astrometry, ellipse_params):
+
+    """
+    Evaluates how well a certain elliptical orbit fits observational data. The difference from :py:meth:`brute_force.eval_chi_2` is that this function figures out the rotation on its own.
+
+    :param astrometry: Observation data
+    :param ellipse_params: Same as keplerian orbit parameters, but without the rotation pivot
+    :return: Value of chi squared
+    """
         
     kop = {}
     for field in ellipse_params:
@@ -250,14 +396,38 @@ def eval_rotation_chi_2(astrometry, ellipse_params):
 
 def energy_from_phase_space_point(fsp,GM=1):
 
+    """
+    Calculates the energy from a position and velocity
+    
+    :param fsp: Phase space point. A dictionary containing the position and velocity
+    :param GM: A product of G (universal constant of gravity) and M (mass of the central gravitating object)
+    :return: Energy
+    """
+
     r = numpy.linalg.norm(fsp['position'])
     return -GM/r+0.5*sqr_norm(fsp['velocity'])
 
 def angular_momentum_from_phase_space_point(fsp):
 
+    """
+    Calculates the angular momentum from a position and velocity
+    
+    :param fsp: Phase space point. A dictionary containing the position and velocity
+    :param GM: A product of G (universal constant of gravity) and M (mass of the central gravitating object)
+    :return: Angular momentum vector
+    """
+
     return numpy.cross(fsp['position'],fsp['velocity'])
 
 def orbital_parameters_from_phase_space_point(fsp,GM=1):
+
+    """
+    Reproduces the Keplerian orbital parameters from a position and velocity
+    
+    :param fsp: Phase space point. A dictionary containing the position and velocity
+    :param GM: A product of G (universal constant of gravity) and M (mass of the central gravitating object)
+    :return: Keplerian orbit parameters
+    """
 
     r = numpy.linalg.norm(fsp['position'])
     l = angular_momentum_from_phase_space_point(fsp)
@@ -292,6 +462,14 @@ def orbital_parameters_from_phase_space_point(fsp,GM=1):
 
 def estimate_initial_parameters(astrometry, GM=1):
 
+    """
+    Estimates the keplerian orbit parameters. Used as initial guess for the minimisation.
+
+    :param astrometry: Astrometric data (proper motion and radial velocity)
+    :param GM: A product of G (universal constant of gravity) and M (mass of the central gravitating object)
+    :return: Keplerian orbit parameters
+    """
+
     vx = numpy.diff(astrometry['x'])/numpy.diff(astrometry['t'])
     vy = numpy.diff(astrometry['y'])/numpy.diff(astrometry['t'])
     vz_mid = mid_array(astrometry['vz'])
@@ -316,6 +494,14 @@ def estimate_initial_parameters(astrometry, GM=1):
 
 def calc_gl_position_block(astrometry, trajectory):
 
+    """
+    Finds the best linear transformation that aligns the theoretical trajectory to the proper motion data
+
+    :param astrometry: Observational data (proper motion and radial velocity)
+    :param trajectory: Complete theoretical trajectory
+    :return: A 2x2 matrix
+    """
+
     s2d_list = trajectory['position'].T[:2].T
     r2d_list = numpy.vstack((astrometry['x'],astrometry['y'])).T
     mat_1 = numpy.einsum('ni,nj',s2d_list,s2d_list)
@@ -325,12 +511,28 @@ def calc_gl_position_block(astrometry, trajectory):
 
 def calc_gl_velocity_block(astrometry, trajectory):
 
+    """
+    Finds the best linear transformation that aligns the theoretical trajectory to the proper radial velocity data
+
+    :param astrometry: Observational data (proper motion and radial velocity)
+    :param trajectory: Complete theoretical trajectory
+    :return: A 2 vector
+    """
+
     v2d_list = trajectory['velocity'].T[:2].T
     mat_1 = numpy.einsum('ni,nj',v2d_list,v2d_list)
     vec_1 = numpy.einsum('n,ni',astrometry['vz'],v2d_list)
     return numpy.dot(numpy.linalg.inv(mat_1),vec_1)
 
 def fit_parameters_wr(astrometry,GM=1):
+
+    """
+    Fits Keplerian orbital parameters to observational data. Uses calculation of the rotation matrix for optimisation. 
+
+    :param astrometry: Observational data (proper motion and radial velocity)
+    :param GM: A product of G (universal constant of gravity) and M (mass of the central gravitating object)
+    :return: Keplerian orbtial parameters
+    """
 
     from scipy.optimize import minimize
 
@@ -363,7 +565,15 @@ Tests
 
 class TestSuite(unittest.TestCase):
 
+    """
+    Test suite for this module
+    """
+
     def testPivot2Generator(self):
+
+        """
+        Verifies the conversion of a pivot vector to a generator
+        """
     
         pivot = numpy.random.rand(3)
         generator = pivot2generator(pivot)
@@ -377,6 +587,10 @@ class TestSuite(unittest.TestCase):
             self.assertEqual(temp[i],0)
             
     def testPivot2Rotation(self):
+
+        """
+        Verifies the conversion of a pivot vector to a rotation matrix
+        """
     
         pivot = numpy.random.rand(3)
         R = pivot2rotation(pivot)
@@ -388,6 +602,10 @@ class TestSuite(unittest.TestCase):
         self.assertTrue(1e-10>sum)
             
     def testTime2MeanAnomalyConversionCircularMotion(self):
+
+        """
+        Verifies the conversion of time to mean anomaly, for the secular case of circular motion
+        """
     
         for i in range(1000):
             kop = {'GM':1.0,
@@ -401,6 +619,10 @@ class TestSuite(unittest.TestCase):
             self.assertAlmostEqual(mean_anomaly,time/timescale)
             
     def testMeanAnomaly2TimeConversionCircularMotion(self):
+
+        """
+        Verifies the conversion of mean anomaly to time for the secular case of circular motion
+        """
     
         for i in range(1000):
             kop = {'GM':1.0,
@@ -414,6 +636,11 @@ class TestSuite(unittest.TestCase):
             self.assertAlmostEqual(mean_anomaly,time/timescale)
             
     def testMeanAnomalyTimeReciprocity(self):
+
+        """
+        Verifies the conversion back and forth between mean and true anomaly
+        """
+
         for i in range(1000):
             kop = {'GM':1.0,
                    'semilatus rectum':numpy.random.rand(),
@@ -426,6 +653,10 @@ class TestSuite(unittest.TestCase):
             self.assertAlmostEqual(mean_anomaly,reconstructed_mean_anomaly)
 
     def testTrajectoryPositionVelocityConsistency(self):
+
+        """
+        Verifies the velocity and position generated are consistent
+        """
 
         kop = {'GM':1,
                'semilatus rectum':0.5*(1+numpy.random.rand()),
@@ -448,6 +679,10 @@ class TestSuite(unittest.TestCase):
             self.assertAlmostEqual(vz,dzdt,places=4)
             
     def testChiSquareEvaluations(self):
+
+        """
+        Verifies that the minimum value of chi squared is only obtained for the original Keplerian orbit parameters
+        """
     
         ref_kop = {'GM':1.0,
                'semilatus rectum':numpy.random.rand(),
@@ -467,6 +702,10 @@ class TestSuite(unittest.TestCase):
             self.assertTrue(chi_2>ref_chi_2)
 
     def testEstimateInitialParameters(self):
+
+        """
+        Verifies the estimation of the initial keplerian parameters
+        """
 
         kop = {'GM':1.0,
                'semilatus rectum':1.0,
@@ -493,6 +732,10 @@ class TestSuite(unittest.TestCase):
                                    places=4)
 
     def testBruteForceParameterFit(self):
+
+        """
+        Verifies that the brute force fit reproduces the Keplerian parameters
+        """
         
         kop = {'GM':1,
                'semilatus rectum':1.5,#numpy.random.rand(),
@@ -513,6 +756,10 @@ class TestSuite(unittest.TestCase):
                                places=2)
 
     def testRotationParameterFit(self):
+
+        """
+        Verifies that the rotation based parameter fit reproduces the original Keplerian parameters
+        """
         
 #        kop = {'GM':1,
 #               'semilatus rectum':numpy.random.rand(),
@@ -539,6 +786,10 @@ class TestSuite(unittest.TestCase):
 
     def testBestCayleyRotation(self):
 
+        """
+        Verifies that the Cayley rotation function reproduces the pivot vector
+        """
+
         x_list = numpy.random.rand(100,3)
         pivot = numpy.random.rand(3)
         rotation = pivot2rotation(pivot)
@@ -548,6 +799,10 @@ class TestSuite(unittest.TestCase):
             self.assertAlmostEqual(p1,p2)
 
     def testCalcGLFit(self):
+
+        """
+        Verifies the calibration of a linear transformation that aligns the theoretical and observational data
+        """
 
         kop = {'GM':1,
                'semilatus rectum':numpy.random.rand(),
@@ -568,6 +823,10 @@ class TestSuite(unittest.TestCase):
 
     def testGeneratorPivotReciprocity(self):
 
+        """
+        Verifies the conversion back and forth between anti symmetric generator and pivot vector
+        """
+
         pivot = numpy.random.rand(3)
         generator = pivot2generator(pivot)
         reproduced = generator2pivot(generator)
@@ -575,6 +834,10 @@ class TestSuite(unittest.TestCase):
             self.assertAlmostEqual(a,b)
 
     def testCalcPivotFromGLRectangleBlock(self):
+
+        """
+        Verifies the calculation of a pivot vector from a general linear transformation
+        """
 
         pivot = numpy.random.rand(3)
         rotation = pivot2rotation(pivot)
@@ -588,6 +851,10 @@ class TestSuite(unittest.TestCase):
 
     def testRotation2PivotReciprocity(self):
 
+        """
+        Verifies the conversion back and forth between pivot vector and rotation matrix
+        """
+
         pivot = numpy.random.rand(3)
         rotation = pivot2rotation(pivot)
         reproduced = rotation2pivot(rotation)
@@ -595,6 +862,10 @@ class TestSuite(unittest.TestCase):
             self.assertAlmostEqual(a,b)
 
     def testFitSmallRotation(self):
+
+        """
+        Verifies the calculation of rotation matrix, in the limit of small rotation angles
+        """
 
         from time import time
 
@@ -612,6 +883,10 @@ class TestSuite(unittest.TestCase):
             self.assertAlmostEqual(a,b)
 
     def testFitRotation(self):
+
+        """
+        Verifies the calculation of a rotation matrix, without assuming small rotation angles
+        """
 
         from time import time
 

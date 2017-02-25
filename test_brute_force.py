cdc67eba86fb2da6f7210c9c9bbdefbef4fd0eb3
Almog Yalinewich <almog.yalin@gmail.com>
2017-02-25 14:53:18 +0200

    fix lint warnings

--- test_brute_force.py
+++ test_brute_force.py
@@ -1,3 +1,7 @@
+"""
+Test suite for the brute force module
+"""
+
 import unittest
 import numpy
 
@@ -7,7 +11,7 @@ class TestSuite(unittest.TestCase):
     Test suite for this module
     """
 
-    def testPivot2Generator(self):
+    def test_pivot_to_generator(self):
 
         """
         Verifies the conversion of a pivot vector to a generator
@@ -27,62 +31,65 @@ class TestSuite(unittest.TestCase):
         for i in range(3):
             self.assertEqual(temp[i], 0)
 
-    def testPivot2Rotation(self):
+    def test_pivot_to_rotation(self):
 
         """
         Verifies the conversion of a pivot vector to a rotation matrix
         """
 
         from brute_force import pivot2rotation
+        from numpy.random import rand
 
-        pivot = numpy.random.rand(3)
-        R = pivot2rotation(pivot)
-        temp = numpy.identity(3) - numpy.dot(numpy.transpose(R), R)
+        pivot = rand(3)
+        rot = pivot2rotation(pivot)
+        temp = numpy.identity(3) - numpy.dot(numpy.transpose(rot), rot)
         res = 0
         for i in range(3):
             for j in range(3):
                 res += temp[i, j]**2
         self.assertTrue(res < 1e-10)
 
-    def testTime2MeanAnomalyConversionCircularMotion(self):
+    def test_circular_motion_t2ma(self):
 
         """
         Verifies the conversion of time to mean anomaly, for the secular case of circular motion
         """
 
         from brute_force import convert_time2mean_anomaly
+        from numpy.random import rand
 
         for _ in range(1000):
             kop = {'GM':1.0,
-                   'semilatus rectum':numpy.random.rand(),
+                   'semilatus rectum':rand(),
                    'eccentricity':0,
                    'periapse time':0,
                    'pivot':[0, 0, 0]}
-            time = numpy.random.rand()
+            time = rand()
             mean_anomaly = convert_time2mean_anomaly(time, kop)
             timescale = 1/numpy.sqrt(kop['GM']/kop['semilatus rectum']**3)
             self.assertAlmostEqual(mean_anomaly, time/timescale)
 
-    def testMeanAnomaly2TimeConversionCircularMotion(self):
+    def test_circular_motion_ma2t(self):
 
         """
         Verifies the conversion of mean anomaly to time for the secular case of circular motion
         """
 
         from brute_force import convert_mean_anomaly2time
+        from numpy.random import rand
 
         for _ in range(1000):
             kop = {'GM':1.0,
-                   'semilatus rectum':numpy.random.rand(),
+                   'semilatus rectum':rand(),
                    'eccentricity':0,
                    'periapse time':0,
                    'pivot':[0, 0, 0]}
-            mean_anomaly = numpy.random.rand()
+            mean_anomaly = rand()
             time = convert_mean_anomaly2time(mean_anomaly, kop)
             timescale = 1/numpy.sqrt(kop['GM']/kop['semilatus rectum']**3)
             self.assertAlmostEqual(mean_anomaly, time/timescale)
 
-    def testMeanAnomalyTimeReciprocity(self):
+    def test_ma2t_reciprocity(self):
 
         """
         Verifies the conversion back and forth between mean and true anomaly
@@ -90,19 +97,20 @@ class TestSuite(unittest.TestCase):
 
         from brute_force import convert_mean_anomaly2time
         from brute_force import convert_time2mean_anomaly
+        from numpy.random import rand
 
         for _ in range(1000):
             kop = {'GM':1.0,
-                   'semilatus rectum':numpy.random.rand(),
-                   'eccentricity':numpy.random.rand(),
-                   'periapse time':numpy.random.rand(),
+                   'semilatus rectum':rand(),
+                   'eccentricity':rand(),
+                   'periapse time':rand(),
                    'pivot':[0, 0, 0]}
-            mean_anomaly = numpy.random.rand()
+            mean_anomaly = rand()
             time = convert_mean_anomaly2time(mean_anomaly, kop)
             reconstructed_mean_anomaly = convert_time2mean_anomaly(time, kop)
             self.assertAlmostEqual(mean_anomaly, reconstructed_mean_anomaly)
 
-    def testTrajectoryPositionVelocityConsistency(self):
+    def test_trajectory_consistency(self):
 
         """
         Verifies the velocity and position generated are consistent
@@ -110,28 +118,27 @@ class TestSuite(unittest.TestCase):
 
         from brute_force import generate_complete_trajectory
         from brute_force import mid_array
+        from numpy.random import rand
 
         kop = {'GM':1,
-               'semilatus rectum':0.5*(1+numpy.random.rand()),
-               'eccentricity':numpy.random.rand(),
-               'periapse time':numpy.random.rand(),
-               'pivot':numpy.random.rand(3)}
+               'semilatus rectum':0.5*(1+rand()),
+               'eccentricity':rand(),
+               'periapse time':rand(),
+               'pivot':rand(3)}
         time_list = numpy.linspace(0, 10, 10000)
-        ct = generate_complete_trajectory(kop, time_list)
-        dxdt_list = numpy.diff(ct['position'].T[0])/numpy.diff(time_list)
-        dydt_list = numpy.diff(ct['position'].T[1])/numpy.diff(time_list)
-        dzdt_list = numpy.diff(ct['position'].T[2])/numpy.diff(time_list)
-        vx_mid = mid_array(ct['velocity'].T[0])
-        vy_mid = mid_array(ct['velocity'].T[1])
-        vz_mid = mid_array(ct['velocity'].T[2])
-        for dxdt, vx in zip(dxdt_list, vx_mid):
-            self.assertAlmostEqual(vx, dxdt, places=4)
-        for dydt, vy in zip(dydt_list, vy_mid):
-            self.assertAlmostEqual(vy, dydt, places=4)
-        for dzdt, vz in zip(dzdt_list, vz_mid):
-            self.assertAlmostEqual(vz, dzdt, places=4)
+        ctr = generate_complete_trajectory(kop, time_list)
+        derivs = {
+            'vx':numpy.diff(ctr['position'].T[0])/numpy.diff(time_list),
+            'vy':numpy.diff(ctr['position'].T[1])/numpy.diff(time_list),
+            'vz':numpy.diff(ctr['position'].T[2])/numpy.diff(time_list)}
+        mid = {'vx':mid_array(ctr['velocity'].T[0]),
+               'vy':mid_array(ctr['velocity'].T[1]),
+               'vz':mid_array(ctr['velocity'].T[2])}
+        for velc in mid:
+            for itm1, itm2 in zip(mid[velc], derivs[velc]):
+                self.assertAlmostEqual(itm1, itm2, places=4)
 
-    def testChiSquareEvaluations(self):
+    def test_chi_square_eval(self):
 
         """
         Verifies that the minimum value of chi squared
@@ -140,25 +147,26 @@ class TestSuite(unittest.TestCase):
 
         from brute_force import generate_astrometry
         from brute_force import eval_chi_2
+        from numpy.random import rand
 
         ref_kop = {'GM':1.0,
-                   'semilatus rectum':numpy.random.rand(),
-                   'eccentricity':0.2*numpy.random.rand(),
-                   'periapse time':numpy.random.rand(),
-                   'pivot':5*numpy.random.rand(3)}
+                   'semilatus rectum':rand(),
+                   'eccentricity':0.2*rand(),
+                   'periapse time':rand(),
+                   'pivot':5*rand(3)}
         time_list = numpy.linspace(0, 10, 100)
         astrometry = generate_astrometry(ref_kop, time_list)
         ref_chi_2 = eval_chi_2(ref_kop, astrometry)
         for _ in range(10):
             kop = {'GM':1.0,
-                   'semilatus rectum':numpy.random.rand(),
-                   'eccentricity':0.2*numpy.random.rand(),
-                   'periapse time':numpy.random.rand(),
-                   'pivot':5*numpy.random.rand(3)}
+                   'semilatus rectum':rand(),
+                   'eccentricity':0.2*rand(),
+                   'periapse time':rand(),
+                   'pivot':5*rand(3)}
             chi_2 = eval_chi_2(kop, astrometry)
             self.assertTrue(chi_2 > ref_chi_2)
 
-    def testEstimateInitialParameters(self):
+    def test_estimate_init_params(self):
 
         """
         Verifies the estimation of the initial keplerian parameters
@@ -166,12 +174,13 @@ class TestSuite(unittest.TestCase):
 
         from brute_force import generate_astrometry
         from brute_force import estimate_initial_parameters
+        from numpy.random import rand
 
         kop = {'GM':1.0,
                'semilatus rectum':1.0,
                'eccentricity':0.5,
                'periapse time':0.2,
-               'pivot':numpy.random.rand(3)}
+               'pivot':rand(3)}
         time_list = numpy.linspace(0, 10, 10000)
         astrometry = generate_astrometry(kop, time_list)
         sol = estimate_initial_parameters(astrometry, grp=kop['GM'])
@@ -190,7 +199,7 @@ class TestSuite(unittest.TestCase):
                                    kop['pivot'][i],
                                    places=4)
 
-    def testBruteForceParameterFit(self):
+    def test_brute_force_fit(self):
 
         """
         Verifies that the brute force fit reproduces the Keplerian parameters
@@ -200,10 +209,10 @@ class TestSuite(unittest.TestCase):
         from brute_force import fit_parameters_bf
 
         kop = {'GM':1,
-               'semilatus rectum':1.5,#numpy.random.rand(),
-               'eccentricity':0.3,#0.9*numpy.random.rand(),
-               'periapse time':0.1,#numpy.random.rand(),
-               'pivot':numpy.array([0.1, -0.2, 0.3])}#numpy.random.rand(3)}
+               'semilatus rectum':1.5,#rand(),
+               'eccentricity':0.3,#0.9*rand(),
+               'periapse time':0.1,#rand(),
+               'pivot':numpy.array([0.1, -0.2, 0.3])}#rand(3)}
         time_list = numpy.linspace(0, 10, 100)
         astrometry = generate_astrometry(kop, time_list)
         sol = fit_parameters_bf(astrometry)
@@ -217,7 +226,7 @@ class TestSuite(unittest.TestCase):
                                kop['periapse time'],
                                places=2)
 
-    def testRotationParameterFit(self):
+    def test_rotation_fit(self):
 
         """
         Verifies that the rotation based parameter fit reproduces the original Keplerian parameters
@@ -227,15 +236,15 @@ class TestSuite(unittest.TestCase):
         from brute_force import fit_parameters_wr
 
 #        kop = {'GM':1,
-#               'semilatus rectum':numpy.random.rand(),
-#               'eccentricity':0.9*numpy.random.rand(),
-#               'periapse time':numpy.random.rand(),
-#               'pivot':numpy.random.rand(3)}
+#               'semilatus rectum':rand(),
+#               'eccentricity':0.9*rand(),
+#               'periapse time':rand(),
+#               'pivot':rand(3)}
         kop = {'GM':1,
-               'semilatus rectum':1.5,#numpy.random.rand(),
-               'eccentricity':0.3,#0.9*numpy.random.rand(),
-               'periapse time':0.1,#numpy.random.rand(),
-               'pivot':numpy.array([0.1, -0.2, 0.3])}#numpy.random.rand(3)}
+               'semilatus rectum':1.5,#rand(),
+               'eccentricity':0.3,#0.9*rand(),
+               'periapse time':0.1,#rand(),
+               'pivot':numpy.array([0.1, -0.2, 0.3])}#rand(3)}
         time_list = numpy.linspace(0, 10, 100)
         astrometry = generate_astrometry(kop, time_list)
         sol = fit_parameters_wr(astrometry)
@@ -249,7 +258,7 @@ class TestSuite(unittest.TestCase):
                                kop['periapse time'],
                                places=2)
 
-    def testBestCayleyRotation(self):
+    def test_best_cayley_rotation(self):
 
         """
         Verifies that the Cayley rotation function reproduces the pivot vector
@@ -264,10 +273,10 @@ class TestSuite(unittest.TestCase):
         rotation = pivot2rotation(pivot)
         y_list = numpy.dot(rotation, x_list.T).T
         reproduced = calc_best_cayley_rotation(x_list, y_list)
-        for p1, p2 in zip(pivot, reproduced):
-            self.assertAlmostEqual(p1, p2)
+        for itm1, itm2 in zip(pivot, reproduced):
+            self.assertAlmostEqual(itm1, itm2)
 
-    def testCalcGLFit(self):
+    def test_calc_gl_fit(self):
 
         """
         Verifies the calibration of a linear
@@ -287,18 +296,18 @@ class TestSuite(unittest.TestCase):
                'periapse time':rand(),
                'pivot':numpy.zeros(3)}
         time_list = numpy.linspace(0, 10, 1000)
-        ct = generate_complete_trajectory(kop, time_list)
+        trj = generate_complete_trajectory(kop, time_list)
         kop['pivot'] = rand(3)
         rotation = pivot2rotation(kop['pivot'])
-        ad = generate_astrometry(kop, time_list)
-        position_block = calc_gl_position_block(ad, ct)
-        velocity_block = calc_gl_velocity_block(ad, ct)
+        amd = generate_astrometry(kop, time_list)
+        blocks = {'position':calc_gl_position_block(amd, trj),
+                  'velocity':calc_gl_velocity_block(amd, trj)}
         for i in range(2):
-            self.assertAlmostEqual(velocity_block[i], rotation[2, i])
+            self.assertAlmostEqual(blocks['velocity'][i], rotation[2, i])
             for j in range(2):
-                self.assertAlmostEqual(position_block[i, j], rotation[i, j])
+                self.assertAlmostEqual(blocks['position'][i, j], rotation[i, j])
 
-    def testGeneratorPivotReciprocity(self):
+    def test_gen_pivot_reciprocity(self):
 
         """
         Verifies the conversion back and forth between anti symmetric generator and pivot vector
@@ -311,10 +320,10 @@ class TestSuite(unittest.TestCase):
         pivot = rand(3)
         generator = pivot2generator(pivot)
         reproduced = generator2pivot(generator)
-        for a, b in zip(pivot, reproduced):
-            self.assertAlmostEqual(a, b)
+        for itm1, itm2 in zip(pivot, reproduced):
+            self.assertAlmostEqual(itm1, itm2)
 
-    def testCalcPivotFromGLRectangleBlock(self):
+    def test_calc_pivot_from_gl_block(self):
 
         """
         Verifies the calculation of a pivot vector from a general linear transformation
@@ -331,10 +340,10 @@ class TestSuite(unittest.TestCase):
         proj[1, 1] = 1
         block = numpy.dot(rotation, proj.T)
         reproduced = calc_pivot_from_gl_block(block)
-        for a, b in zip(pivot, reproduced):
-            self.assertAlmostEqual(a, b)
+        for itm1, itm2 in zip(pivot, reproduced):
+            self.assertAlmostEqual(itm1, itm2)
 
-    def testRotation2PivotReciprocity(self):
+    def test_rot_pivot_reciprocity(self):
 
         """
         Verifies the conversion back and forth between pivot vector and rotation matrix
@@ -347,10 +356,10 @@ class TestSuite(unittest.TestCase):
         pivot = rand(3)
         rotation = pivot2rotation(pivot)
         reproduced = rotation2pivot(rotation)
-        for a, b in zip(pivot, reproduced):
-            self.assertAlmostEqual(a, b)
+        for itm1, itm2 in zip(pivot, reproduced):
+            self.assertAlmostEqual(itm1, itm2)
 
-    def testFitSmallRotation(self):
+    def test_fit_small_rotation(self):
 
         """
         Verifies the calculation of rotation matrix, in the limit of small rotation angles
@@ -367,14 +376,14 @@ class TestSuite(unittest.TestCase):
                'periapse time':rand(),
                'pivot':numpy.zeros(3)}
         time_list = numpy.linspace(0, 10, 1000)
-        ct = generate_complete_trajectory(kop, time_list)
+        trj = generate_complete_trajectory(kop, time_list)
         kop['pivot'] = 1e-4*rand(3)
-        ad = generate_astrometry(kop, time_list)
-        reproduced = fit_small_rotation(ad, ct)
-        for a, b in zip(kop['pivot'], reproduced):
-            self.assertAlmostEqual(a, b)
+        amd = generate_astrometry(kop, time_list)
+        reproduced = fit_small_rotation(amd, trj)
+        for itm1, itm2 in zip(kop['pivot'], reproduced):
+            self.assertAlmostEqual(itm1, itm2)
 
-    def testFitRotation(self):
+    def test_fit_rotation(self):
 
         """
         Verifies the calculation of a rotation matrix, without assuming small rotation angles
@@ -391,12 +400,12 @@ class TestSuite(unittest.TestCase):
                'periapse time':rand(),
                'pivot':numpy.zeros(3)}
         time_list = numpy.linspace(0, 10, 1000)
-        ct = generate_complete_trajectory(kop, time_list)
+        trj = generate_complete_trajectory(kop, time_list)
         kop['pivot'] = rand(3)
-        ad = generate_astrometry(kop, time_list)
-        reproduced = fit_rotation_to_astrometry(ad, ct, n_itr=3)
-        for a, b in zip(kop['pivot'], reproduced):
-            self.assertAlmostEqual(a, b)
+        amd = generate_astrometry(kop, time_list)
+        reproduced = fit_rotation_to_astrometry(amd, trj, n_itr=3)
+        for itm1, itm2 in zip(kop['pivot'], reproduced):
+            self.assertAlmostEqual(itm1, itm2)
 
 if __name__ == '__main__':
 
